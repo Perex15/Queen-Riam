f6bf72e6366cd48f072a6d7539bd7bb8425f802c
Perex15 <149716430+Perex15@users.noreply.github.com>
2026-01-09 13:59:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ const settings = {
   
 
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.0",
 };
